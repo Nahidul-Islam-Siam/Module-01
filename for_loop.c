ed902390d84430e9461143356a5ce74e3224b038
Nahidul Islam Siam <siamnahidul093@gmail.com>
2026-04-10 20:47:00 +0600

    Add variables.c to demonstrate input and output of different data types
    - Created a new file variables.c
    - Implemented a program to read an integer, a long long integer, a float, and a character from user input
    - Added corresponding output statements to display the values read

---
+++ for_loop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 100000; i >= 100; i--)
+    {
+        printf("%d I Love u\n", i);
+    }
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,32 @@
+// You've learned about variables, right? Now its time to practice them. You need to take an integer A, a very big integer B, a floating value  C and a character D as input and output them serially.
+
+
+#include <stdio.h>
+int main()
+{
+       int A;
+       long long B;
+       float C;
+       char D;
+       scanf("%d %lld %f %c",&A, &B, &C, &D);
+      printf("First line will contain %d",A);
+         printf("Second line will contain %lld",B);
+       printf("Third line contain %f",C);
+         printf("Fourth line will contain %c",D);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
